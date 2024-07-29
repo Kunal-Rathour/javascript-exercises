3322882c027be9666255cd9a883a5543968de286
Kunal-Rathour <knl.rathour@gmail.com>
2024-07-30 01:47:24 +0530

    Completed javascript exercise 01 and 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function (repeatText, repeatTimes) {
+    //Non looping method
+    // return repeatText.repeat(repeatTimes);
+    //Looping Method
+    if (repeatTimes < 0) {
+        return "ERROR";
+    }
+    else {
+        let newText = "";
+        for (let i = 1; i <= repeatTimes; i++) {
+            newText = newText + repeatText;
+        }
+        return newText;
+    }
 };
-
 // Do not edit below this line
 module.exports = repeatString;
