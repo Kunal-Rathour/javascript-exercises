d002e329a8f51a875d05b9b79d7e5fd62e2677f8
Kunal-Rathour <knl.rathour@gmail.com>
2024-07-31 00:55:24 +0530

    completed javascript exercise 7 temperature conversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (tempInFahrenheit) {
+  let tempInCelcius = Math.round(((tempInFahrenheit - 32) / (9 / 5)) * 10) / 10;
+  return tempInCelcius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (tempInCelcius) {
+  let tempInFahrenheit = Math.round((tempInCelcius * (9 / 5) + 32) * 10) / 10;
+  return tempInFahrenheit;
 };
-
+console.log(convertToCelsius(100));
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
