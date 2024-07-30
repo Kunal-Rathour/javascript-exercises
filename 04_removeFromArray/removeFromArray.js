ab47d3953dca035a9696dfffbd72786a6d32906d
Kunal-Rathour <knl.rathour@gmail.com>
2024-07-30 22:29:06 +0530

    completed javascript exercise 04 remove element from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,19 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function () {
+    let array = arguments[0];
+    // console.log(arguments);
 
+    let index;
+    let numberOfElementToRemove = arguments.length - 1;
+    for (let i = 1; i <= numberOfElementToRemove; i++) {
+        index = array.indexOf(arguments[i]);
+        // console.log(array[index]);
+        while (array.includes(arguments[i])) {
+            array.splice(index, 1);
+        }
+    }
+    return array;
+
+};
+console.log(removeFromArray([1, 2, 3, 4, 5, 6], 2));
 // Do not edit below this line
 module.exports = removeFromArray;
