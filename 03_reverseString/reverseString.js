1bc95286390570b271d252347a3da4d8d2a3a30f
Kunal-Rathour <knl.rathour@gmail.com>
2024-07-30 21:11:08 +0530

    completed javascript exercise 03 reversing string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,16 @@
-const reverseString = function() {
+// const reverseString = function () {
 
-};
-
+// };
+function reverseString(stringToReverse) {
+    let reversedString = "";
+    stringLength = stringToReverse.length;
+    for (let i = 0; i <= stringLength - 1; i++) {
+        reversedString += stringToReverse[stringLength - i - 1];
+    }
+    return reversedString;
+    // reverseString = reverseString + reverseString(stringToReverse[stringLength - 1]);
+    // return reverseString[stringLength - 1];
+}
+console.log(reverseString("HELLO"));
 // Do not edit below this line
 module.exports = reverseString;
